0821fa6dc16f9fc193c5c472f6244d4c7ffa7a51
Jkierem <juanmig8@hotmail.com>
2018-09-27 22:13:26 -0500

    Fixed spacing

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 import { observer , inject } from 'mobx-react'
 import { Form , Label } from 'juanform'
 import { ErrorBanner } from '../../components'
 import { Button , Input , Field } from '../../customForm'
 import { LOGIN_ERROR } from '../../stores/constants'
+
+const WhiteSpace = styled.div`
+	margin: 20px;
+`
 
 @inject("store")
 @observer
@@ -33,6 +38,7 @@ class Login extends React.Component{
 					<Label>What is your password?</Label>
 					<Input name={"password"} type={"password"}/>
 				</Field>
+				<WhiteSpace></WhiteSpace>
 				<Field>
 					<Button submit>Enter</Button>
 				</Field>
